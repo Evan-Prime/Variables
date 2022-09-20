e0dd7eb5ca704fe29f246badb9fb8cdacba96982
Evan MacDonald <evan.t.macdonald01@gmail.com>
2022-09-20 08:52:25 -0300

    Initial commit

---
+++ HelloWorld3-GitHub/HelloWorld3-GitHub/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld3_GitHub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("+-------------------+");
+            Console.WriteLine("|Hello World 3 ! ! !|");
+            Console.WriteLine("+-------------------+");
+            Console.WriteLine("");
+            Console.ReadKey(true);
+        }
+    }
+}
